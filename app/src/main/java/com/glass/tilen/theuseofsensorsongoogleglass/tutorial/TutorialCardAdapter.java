97c012fdc4f67e07283107c7688450ab9c68ea03
tilenskr <skrinjar.tilen@gmail.com>
2015-07-15 23:29:58 +0200

    Created Tutorial classes (activity and adapter) for implementation.

---
+++ app/src/main/java/com/glass/tilen/theuseofsensorsongoogleglass/tutorial/TutorialCardAdapter.java
@@ -0,0 +1,31 @@
+package com.glass.tilen.theuseofsensorsongoogleglass.tutorial;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.glass.widget.CardScrollAdapter;
+
+/**
+ * Created by Tilen on 14.7.2015.
+ */
+public class TutorialCardAdapter extends CardScrollAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public int getPosition(Object o) {
+        return 0;
+    }
+}
