8e6761d3941ce639cbe4e4bea0fd5cc39dc8e58a
tilenskr <skrinjar.tilen@gmail.com>
2015-08-14 13:21:38 +0200

    Update MainSensorManager to use one sensors at once

--- app/src/main/java/com/glass/tilen/theuseofsensorsongoogleglass/sensors/manager/MainSensorManager.java
+++ app/src/main/java/com/glass/tilen/theuseofsensorsongoogleglass/sensors/manager/MainSensorManager.java
@@ -2,18 +2,29 @@ package com.glass.tilen.theuseofsensorsongoogleglass.sensors.manager;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+import com.glass.tilen.theuseofsensorsongoogleglass.settings.Global;
+
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by Tilen on 9.8.2015.
  */
-public class MainSensorManager {
-    private Context mContext;
+public class MainSensorManager implements SensorEventListener {
     private static SensorManager mSensorManager = null;
-
     private static MainSensorManager ourInstance = null;
+    private Sensor mSensors;
+    private MainSensorManagerCallback mCallback;
+
+    public interface MainSensorManagerCallback
+    {
+        void onSensorValueChanged(float[] values);
+    }
+
 
     public static MainSensorManager getInstance(Context mContext) {
         if(ourInstance == null)
@@ -24,16 +35,41 @@ public class MainSensorManager {
     }
 
     private MainSensorManager(Context mContext) {
-        this.mContext = mContext;
         mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
     }
 
+    public void setSensor(int sensorType, MainSensorManagerCallback mCallback )
+    {
+        mSensors = mSensorManager.getDefaultSensor(sensorType);
+        Global.SpeechDebug("MainSensorManager.setSensor(): Maximum sensor value: " + mSensors.getMaximumRange());
+        this.mCallback = mCallback;
+    }
 
+    public void registerSensor()
+    {
+        mSensorManager.registerListener(this, mSensors, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    public void unRegisterSensor()
+    {
+        mSensorManager.unregisterListener(this);
+    }
     public static List<Sensor> getAllSensors(Context mContext)
     {
         // just to be sure if you call getAllSensors, before class is initialized
         if(mSensorManager == null)
             mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
         return mSensorManager.getSensorList(Sensor.TYPE_ALL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Global.SensorsDebug("MainSensorManager.onSensorChanged(): values " + Arrays.toString(event.values));
+        mCallback.onSensorValueChanged(event.values);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Global.SensorsDebug("MainSensorManager.onAccuracyChanged(): accuracy: " + accuracy);
     }
 }
