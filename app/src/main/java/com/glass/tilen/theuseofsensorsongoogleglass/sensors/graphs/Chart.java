cea46512ba4825bbdbe55a1c33be560a6ca4d516
tilenskr <skrinjar.tilen@gmail.com>
2015-08-19 21:12:58 +0200

    Update Chart with updated and new methods

--- app/src/main/java/com/glass/tilen/theuseofsensorsongoogleglass/sensors/graphs/Chart.java
+++ app/src/main/java/com/glass/tilen/theuseofsensorsongoogleglass/sensors/graphs/Chart.java
@@ -1,25 +1,32 @@
 package com.glass.tilen.theuseofsensorsongoogleglass.sensors.graphs;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import com.glass.tilen.theuseofsensorsongoogleglass.R;
 
 /**
  * Created by Tilen on 19.8.2015.
  */
 public class Chart {
+    private Context mContext;
     private LineChart mChart;
+    /** 0 - only X-axis, 1 - only Y-axis, 2 - only Z-Axis, 3 - all axes **/
+    private int axis;
 
-
-    public Chart(LineChart mChart)
+    public Chart(LineChart mChart, Context mContext)
     {
         this.mChart = mChart;
+        this.mContext = mContext;
+        axis = 3;
         setChart();
     }
 
@@ -29,8 +36,10 @@ public class Chart {
         // no description text
         mChart.setDescription("");
         mChart.setDrawGridBackground(false);
-        mChart.setHighlightEnabled(true);
-
+        mChart.setDragEnabled(false);
+        mChart.setScaleEnabled(false);
+        mChart.setPinchZoom(false);
+        mChart.setTouchEnabled(false);
         /** LineData **/
         LineData data = new LineData();
        data.setValueTextColor(Color.WHITE);
@@ -40,7 +49,7 @@ public class Chart {
         /* Legend (only possible after setting data)*/
         Legend l = mChart.getLegend();
         // modify the legend ...
-        // l.setPosition(LegendPosition.LEFT_OF_CHART);
+        l.setPosition(Legend.LegendPosition.BELOW_CHART_RIGHT);
         l.setForm(Legend.LegendForm.LINE);
         l.setTextColor(Color.WHITE);
 
@@ -51,12 +60,12 @@ public class Chart {
         xl.setAvoidFirstLastClipping(true);
         xl.setSpaceBetweenLabels(5);
         xl.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xl.setEnabled(true);
+        xl.setEnabled(false);
 
         YAxis leftAxis = mChart.getAxisLeft();
         leftAxis.setTextColor(Color.WHITE);
-        leftAxis.setAxisMaxValue(100f);
-        leftAxis.setAxisMinValue(0f);
+        leftAxis.setAxisMaxValue(1);
+        leftAxis.setAxisMinValue(-1);
         leftAxis.setStartAtZero(false);
         leftAxis.setDrawGridLines(false);
 
@@ -64,40 +73,58 @@ public class Chart {
         rightAxis.setEnabled(false);
 
         /** LineDataSet **/
-
-
+        Resources resources = mContext.getResources();
+        LineDataSet dataSetX = createDataSet(resources.getString(R.string.x_axis),
+                resources.getColor(R.color.x_axis));
+        LineDataSet dataSetY = createDataSet(resources.getString(R.string.y_axis),
+                resources.getColor(R.color.y_axis));
+        LineDataSet dataSetZ = createDataSet(resources.getString(R.string.z_axis),
+                resources.getColor(R.color.z_axis));
+        data.addDataSet(dataSetX);
+        data.addDataSet(dataSetY);
+        data.addDataSet(dataSetZ);
     }
 
-    private LineDataSet createDataSet(String name, int linecolor, int circleColor)
+    private LineDataSet createDataSet(String name, int lineColor)
     {
         LineDataSet set = new LineDataSet(null, name);
         set.setAxisDependency(YAxis.AxisDependency.LEFT);
-        set.setColor(ColorTemplate.getHoloBlue());
-        set.setCircleColor(Color.WHITE);
+        set.setColor(lineColor);
+        set.setCircleColor(lineColor);
         set.setLineWidth(2f);
-        set.setCircleSize(4f);
+        set.setCircleSize(3);
         set.setFillAlpha(65);
-        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setFillColor(lineColor);
         set.setDrawValues(false);
-
         return set;
     }
 
     public void addSensorValues(float[] values)
     {
         LineData lineData = mChart.getData();
-        LineDataSet lineDataSet;
+        lineData.addXValue(String.valueOf(lineData.getXValCount()));
         for(int i = 0; i < 3; i++)
         {
-            lineDataSet = lineData.getDataSetByIndex(i);
-            // TODO add single value to set line.
+            if(axis != i && axis != 3) {
+                LineDataSet mLineDataSet = lineData.getDataSetByIndex(i);
+                mLineDataSet.clear();
+                continue;
+            }
+            lineData.addEntry(new Entry(values[i], (lineData.getXValCount() - 1)), i);
         }
+
         mChart.notifyDataSetChanged();
         // limit the number of visible entries
-        mChart.setVisibleXRangeMaximum(120);
-        // move to the latest entry
-        mChart.moveViewToX(lineData.getXValCount() - 121);
+        mChart.setVisibleXRangeMaximum(20);
 
+        // move to the latest entry
+        mChart.moveViewToX(lineData.getXValCount() - 21);
+    }
+
+    public void changeAxis()
+    {
+        axis++;
+        axis %= 4;
     }
 
 }
